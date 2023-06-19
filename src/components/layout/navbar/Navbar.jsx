2d3f7ade35407284bf5e2036171688be4a53abcf
Sofía J <sofiajimglez@gmail.com>
2023-06-19 11:14:39 +0200

    feat: adds logo to navbar  and changes favicon

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,15 +1,13 @@
 import './navbar.css';
+import logo from '../../../assets/flag-football.png';
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container">
-        <a className="navbar-brand" href="/">
-          <h1 className="fs-2 mb-0 text-white">
-            <i className="fa-solid fa-person-walking me-2"></i>
-            <i className="fa-solid fa-circle fa-2xs me-3"></i>
-            Football Data
-          </h1>
+        <a className="navbar-brand d-flex gap-2 align-items-center" href="/">
+          <img src={logo} height="40" alt="football data logo" />
+          <h1 className="fs-2 mb-0 text-white">Football Data</h1>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
           <i className="fa-solid fa-grip-lines text-white"></i>
@@ -21,6 +19,9 @@ export default function Navbar() {
             </li>
             <li className="nav-item">
               <a className="nav-link" aria-current="page" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="https://github.com/sofiajimglez/football-data" target="_blank" rel="noreferrer">GitHub Repo</a>
             </li>
           </ul>
         </div>
