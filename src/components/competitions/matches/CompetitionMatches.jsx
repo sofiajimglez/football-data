036808eda9a1766aec35ed2c5bcb121a1f2faaa7
Sofía J <sofiajimglez@gmail.com>
2023-06-20 13:22:13 +0200

    refactor: async-await in useEffect & adds error message

--- src/components/competitions/matches/CompetitionMatches.jsx
+++ src/components/competitions/matches/CompetitionMatches.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import competitionsService from "../../../services/competitions.service";
 import CompetitionHeader from "../header/CompetitionHeader";
 import MatchdayGroup from "./MatchdayGroup";
@@ -8,40 +8,53 @@ export default function CompetitionMatches() {
   const { code } = useParams();
   const [competition, setCompetition] = useState({});
   const [matches, setMatches] = useState([]);
+  const [fetchError, setFetchError] = useState('');
 
   useEffect(() => {
-    competitionsService.detail(code)
-      .then(res => setCompetition(res))
-      .catch(console.error)
+    async function fetchData() {
+      try {
+        const competitionData = await competitionsService.detail(code);
+        setCompetition(competitionData);
+
+        const matchesData = await competitionsService.matches(competitionData.id);
+
+        // Groups the array of total matches by matchday
+        const matchesByMatchday = matchesData.matches.reduce((result, match) => {
+          const { matchday } = match;
+          if (!result[matchday]) {
+            result[matchday] = [];
+          }
+          result[matchday].push(match);
+          return result;
+        }, {});
+
+        // Extracts result in one array of arrays. Each one is a matchday with the corresponding matches
+        const matchesByMatchdayArr = Object.values(matchesByMatchday);
+        setMatches(matchesByMatchdayArr);
+        
+      } catch (error) {
+        console.error(error);
+        setFetchError(error.message);
+      }
+    }
+
+    setFetchError('');
+    fetchData();
+
   }, [code]);
-
-  useEffect(() => {
-    if (competition.id) {
-      competitionsService.matches(competition.id)
-        .then(res => setMatches(res.matches))
-        .catch(console.error)
-    }
-  }, [competition.id]);
-
-  // Groups the array of total matches by matchday
-  const groupedMatches = matches.reduce((result, match) => {
-    const { matchday } = match;
-    if (!result[matchday]) {
-      result[matchday] = [];
-    }
-    result[matchday].push(match);
-    return result;
-  }, {});
-
-  // Extracts result in one array of arrays. Each one is a matchday with the corresponding matches
-  const arrsByMatchday = Object.values(groupedMatches);
 
   if (matches.length === 0) return (<p>Loading...</p>);
 
   return (
     <div>
       <CompetitionHeader competition={competition} />
-      {arrsByMatchday.map((matchday, i) => <MatchdayGroup day={i + 1} matches={matchday} key={i} />)}
+
+      {fetchError && (<div className="alert alert-danger" role="alert">
+        <p>An error occurred while loading the matches! 😥 Please, try again later.</p>
+        <br /><small>{fetchError}</small>
+      </div>)}
+
+      {matches.map((matchday, i) => <MatchdayGroup day={i + 1} matches={matchday} key={i} />)}
     </div>
   )
 }
--- src/components/competitions/standings/CompetitionStandings.jsx
+++ src/components/competitions/standings/CompetitionStandings.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import competitionsService from "../../../services/competitions.service";
 import CompetitionHeader from "../header/CompetitionHeader";
 import StandingItem from "./StandingItem";
@@ -9,32 +9,43 @@ export default function CompetitionStandings() {
   const { code } = useParams();
   const [competition, setCompetition] = useState({});
   const [standings, setStandings] = useState([]);
+  const [fetchError, setFetchError] = useState('');
 
   useEffect(() => {
-    competitionsService.detail(code)
-      .then(res => setCompetition(res))
-      .catch(console.error)
+    async function fetchData() {
+      try {
+        const competitionData = await competitionsService.detail(code);
+        setCompetition(competitionData);
+
+        const standingsData = await competitionsService.standings(competitionData.id);
+        const standingsTable = standingsData.standings.map(elem => elem.table).flat();
+        setStandings(standingsTable);
+
+      } catch (error) {
+        console.error(error);
+        setFetchError(error.message);
+      }
+    }
+
+    setFetchError('');
+    fetchData();
+
   }, [code]);
 
-  useEffect(() => {
-    if (competition.id) {
-      competitionsService.standings(competition.id)
-        .then(res => {
-          const data = res.standings.map(elem => elem.table).flat();
-          setStandings(data);
-        })
-        .catch(console.error);
-    }
-  }, [competition.id]);
+  console.log(standings)
 
   if (standings.length === 0) return (<p>Loading...</p>);
-
-  console.log(standings)
 
   return (
     <div>
       <CompetitionHeader competition={competition} />
-      {standings.map(standing => <StandingItem key={standing.id} data={standing} />)}
+
+      {fetchError && (<div className="alert alert-danger" role="alert">
+        <p>An error occurred while loading the matches! 😥 Please, try again later.</p>
+        <br /><small>{fetchError}</small>
+      </div>)}
+
+      {standings.map(standing => <StandingItem data={standing} key={standing.position} />)}
     </div>
   )
 }
