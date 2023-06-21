97de24a29a2577897e161d378cf27b7a7def4e0e
Sofía J <sofiajimglez@gmail.com>
2023-06-21 21:31:08 +0200

    feat: adds styles and content to header component

--- src/components/competitions/header/CompetitionHeader.jsx
+++ src/components/competitions/header/CompetitionHeader.jsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import dayjs from 'dayjs';
+import './header.css';
 
 export default function CompetitionHeader({ competition }) {
+
+  const startDate = dayjs(competition.currentSeason.startDate);
+  const startYear = startDate.year();
+  const endDate = dayjs(competition.currentSeason.endDate);
+  const endYear = endDate.year();
 
   if (!competition) return (<p>Loading...</p>);
 
   return (
-    <div>
-      <h1>{competition.name}</h1>
-      <div className="d-flex gap-2 mb-3">
-        <Link to={`/competitions/${competition.code}`}>See seasons</Link>
-        <Link to={`/competitions/${competition.code}/standings`}>See standings</Link>
-        <Link to={`/competitions/${competition.code}/matches`}>See matches</Link>
+    <div className="competition-header d-flex flex-column flex-sm-row flex-wrap gap-4 align-items-center">
+      <div>
+        <img src={competition.emblem} alt={`${competition.name} logo`} width={120} />
+      </div>
+      <div className="d-flex flex-wrap flex-row flex-sm-column">
+        <h1>{competition.name}</h1>
+        <div className="d-flex flex-wrap align-items-center gap-2 mt-1">
+          <img src={competition.area.flag} alt={`${competition.area.name} logo`} height={20} />
+          <p className="mb-0 me-2">{competition.area.name}</p>
+          <p className="mb-0"> <strong>Current season:</strong> {startYear}/{endYear}</p>
+          <p className="mb-0"> <strong>Current matchday:</strong> {competition.currentSeason.currentMatchday}</p>
+        </div>
+        <div className="d-flex flex-wrap gap-3 mt-3">
+          <Link to={`/competitions/${competition.code}`} className="btn btn-main">Seasons</Link>
+          <Link to={`/competitions/${competition.code}/standings`}  className="btn btn-main">Standings</Link>
+          <Link to={`/competitions/${competition.code}/matches`}  className="btn btn-main">Matches</Link>
+        </div>
       </div>
     </div>
   )
